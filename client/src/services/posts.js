89d413b71e2408a7f55024f8a7e57ebcdc8bb631
Ray Morrison <rlmorrison.74@gmail.com>
2021-11-03 17:01:18 -0400

    users and posts api calls written on front end

---
+++ client/src/services/posts.js
@@ -0,0 +1,26 @@
+import api from "./api-config";
+
+export const getAllPosts = async () => {
+  const res = await api.get("/posts");
+  return res.data;
+};
+
+export const getOnePost = async (id) => {
+  const res = await api.get(`/posts/${id}`);
+  return res.data;
+};
+
+export const createPost = async (postData) => {
+  const res = await api.post("/posts", { post: postData });
+  return res.data;
+};
+
+export const updatePost = async (id, postData) => {
+  const res = await api.put(`/foods/${id}`, { post: postData });
+  return res.data;
+};
+
+export const deletePost = async (id) => {
+    const res = await api.delete(`/foods/${id}`)
+    return res
+}
