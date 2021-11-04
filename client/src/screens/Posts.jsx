bfbdec4ce574a4a12da4ec086b2c4f9844716d3f
Ray Morrison <rlmorrison.74@gmail.com>
2021-11-04 10:12:05 -0400

    posts displaying on Posts.jsx with usernames and comment counts

--- client/src/screens/Posts.jsx
+++ client/src/screens/Posts.jsx
@@ -1,7 +1,26 @@
-export default function Posts() {
-    return (
-        <div>
-            
+import { Link } from "react-router-dom";
+
+export default function Posts({ posts, handlePostDelete }) {
+  console.log(posts);
+  return (
+    <div>
+      <h3>Posts</h3>
+      {posts.map((post) => (
+        <div key={post.id}>
+          <Link to={`/posts/${post.id}`}>
+                  <p>{post.subject}</p>
+          </Link>
+              <p>{post.user.username}</p>
+              <p>{post.comments.length} comments</p>
+          <Link to={`/posts/${post.id}/edit`}>
+            <button>Edit</button>
+          </Link>
+          <button onClick={() => handlePostDelete(post.id)}>Delete</button>
         </div>
-    )
+      ))}
+          <Link to='/posts/new'>
+              <button>Create</button>
+          </Link>
+    </div>
+  );
 }
