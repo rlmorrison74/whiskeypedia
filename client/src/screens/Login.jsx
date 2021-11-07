39b9acc01e4375e24193016555805001818af7e8
Ray Morrison <rlmorrison.74@gmail.com>
2021-11-07 10:35:10 -0500

    login form styled

--- client/src/screens/Login.jsx
+++ client/src/screens/Login.jsx
@@ -1,6 +1,7 @@
 import { TextField, Button } from "@mui/material";
 import { useState } from "react";
 import { Link } from "react-router-dom";
+import "../styles/Screens/login.css";
 
 export default function Login({ handleLogin }) {
   const [formData, setFormData] = useState({
@@ -18,36 +19,39 @@ export default function Login({ handleLogin }) {
   };
 
   return (
-    <form
-      onSubmit={(e) => {
-        e.preventDefault();
-        handleLogin(formData);
-      }}
-    >
-      <TextField
-        id="username"
-        type="text"
-        autoFocus
-        label="Username"
-        value={username}
-        name="username"
-        onChange={handleChange}
-      />
-      <br />
-      <TextField
-        id="password"
-        type="password"
-        label="Password"
-        value={password}
-        name="password"
-        onChange={handleChange}
-      />
-      <br />
-      <Button type="submit" children="Submit" variant="contained" />
-      <p>
-        {" "}
-        Don't have an account? Create one <Link to="/register">here</Link>.
-      </p>
-    </form>
+    <div className="login">
+      <form
+        className="loginform"
+        onSubmit={(e) => {
+          e.preventDefault();
+          handleLogin(formData);
+        }}
+      >
+        <TextField
+          id="username"
+          type="text"
+          autoFocus
+          label="Username"
+          value={username}
+          name="username"
+          onChange={handleChange}
+        />
+        <br />
+        <TextField
+          id="password"
+          type="password"
+          label="Password"
+          value={password}
+          name="password"
+          onChange={handleChange}
+        />
+        <br />
+        <Button type="submit" children="Submit" variant="contained" />
+        <p>
+          {" "}
+          Don't have an account? Create one <Link className="registerlink" to="/register">here</Link>.
+        </p>
+      </form>
+    </div>
   );
 }
